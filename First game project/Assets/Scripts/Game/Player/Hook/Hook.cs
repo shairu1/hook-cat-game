6b18da8ab931791421a89ed0aaff201d22972360
shairu1 <shai.ru1@yandex.ru>
2022-10-02 21:45:01 +0300

    new unity version, add localizations

--- First game project/Assets/Scripts/Game/Player/Hook/Hook.cs
+++ First game project/Assets/Scripts/Game/Player/Hook/Hook.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Hook : MonoBehaviour
 {
--- First game project/Assets/Scripts/Libraries/Networking.cs
+++ First game project/Assets/Scripts/Libraries/Networking.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
 using System.Net;
-using UnityEngine;
 
 
 namespace Networking
